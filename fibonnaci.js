1fcc613f1f8d06a20fd97d8b1166314031e599e2
WeeMellowToby <69983951+WeeMellowToby@users.noreply.github.com>
2021-10-25 12:17:08 +0100

    allows user to choose the amount of numbers to run through

--- fibonnaci.js
+++ fibonnaci.js
@@ -1,11 +1,17 @@
 var numberDisplay = document.getElementById("numbers");
+
 var number1 = 1;
 var number2 = 1;
 var fibonnaciString = ""
-for (let i = 0; i < 5; i++) {
-    let tempnumber = number2;
-    number2 = tempnumber + number1
-    number1 = tempnumber;
-    fibonnaciString += number2 + " "
+
+
+  function numberSubmitted() {
+    let amount = document.getElementById("fname").value;
+    for (let i = 0; i < amount; i++) {
+        let tempnumber = number2;
+        number2 = tempnumber + number1
+        number1 = tempnumber;
+        fibonnaciString += number2 + " "
+      }
+      numberDisplay.innerHTML = fibonnaciString;
   }
-  numberDisplay.innerHTML = fibonnaciString;
